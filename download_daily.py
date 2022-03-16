b3d0d438f044ab8d052f92463ec89035e0952869
Jon Paul Miles <codingfriend1@gmail.com>
2022-03-16 16:28:55 -0500

    Completes initial logic for downloading daily data.

--- download_daily.py
+++ download_daily.py
@@ -16,6 +16,8 @@ from download import download_from_url, download_and_extract_from_url
 
 UNEXTRACTED_DAILY_FILE = 'ghcnd_all.tar.gz'
 EXTRACTED_DAILY_FOLDER = 'ghcnd_all'
+WEB_URL_FOR_ALL_DAILY_DATA = 'https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd_all.tar.gz'
+WEB_URL_DAILY_DATA_VERSION_FILE = 'https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-version.txt'
 
 def download_from_url(url, file_name):
   urllib.request.urlretrieve(url, file_name)
@@ -32,8 +34,8 @@ def get_version():
   FILE_PATH = os.path.join('.', FILE_NAME)
 
   if not os.path.exists(os.path.join('.', FILE_PATH)):
-    print(f"Downloading Version file: https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-version.txt as {FILE_NAME}")
-    version_file = download_from_url('https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-version.txt', FILE_NAME)
+    print(f"Downloading Version file: {WEB_URL_DAILY_DATA_VERSION_FILE} as {FILE_NAME}")
+    version_file = download_from_url(WEB_URL_DAILY_DATA_VERSION_FILE, FILE_NAME)
 
   version_text = open(FILE_PATH, 'r').read()
 
@@ -42,7 +44,7 @@ def get_version():
   return version_name
 
 def scan_for_daily_data_folder():
-  
+
   daily_data_folder = glob.glob(os.path.join('.', EXTRACTED_DAILY_FOLDER))
 
   return daily_data_folder
@@ -60,16 +62,16 @@ def get_daily_data():
     print(f"Folder not found. Checking if {UNEXTRACTED_DAILY_FILE} exists in current directory...\n")
 
     if not os.path.exists(os.path.join('.', UNEXTRACTED_FILE_PATH)):
-      print(f"Folder not found. Downloading and extracting GHCN daily {VERSION} datasets from NOAA...\n")
+      print(f"Unextracted file {UNEXTRACTED_DAILY_FILE} not found. Downloading and extracting GHCN daily {UNEXTRACTED_DAILY_FILE} datasets from NOAA...\n")
 
-      print(f"Downloading GHCN Daily data: https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd_all.tar.gz")
-      download_and_extract_from_url('https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd_all.tar.gz')
+      print(f"Downloading GHCN Daily data: {WEB_URL_FOR_ALL_DAILY_DATA}")
+      download_and_extract_from_url(WEB_URL_FOR_ALL_DAILY_DATA)
 
       print(f"\nSuccessfully downloaded:")
       print(f"  https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd_all.tar.gz")
 
     else:
-      print(f"Folder unextracted {UNEXTRACTED_DAILY_FILE}. Extracting now...\n")
+      print(f"Found unextracted {UNEXTRACTED_DAILY_FILE}. Extracting now...\n")
       ghcnd_data = tarfile.open(UNEXTRACTED_FILE_PATH, mode="r|gz")
       ghcnd_data.extractall()
 
@@ -84,5 +86,7 @@ def get_daily_data():
     print(f"============\n")
     print(f"  {daily_data_folder[0]}")
     print('\n  No downloads necessary.')
+
+  print('')
 
   return daily_data_folder[0]
